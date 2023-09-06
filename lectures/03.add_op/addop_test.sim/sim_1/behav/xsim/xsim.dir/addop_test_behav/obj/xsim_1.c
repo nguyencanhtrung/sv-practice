dd1a5dd5ff43e4196cb04b52c6fb866f0eabefe9
Trung Nguyen <nguyencanhtrung@me.com>
2023-09-06 11:29:34 +0700

    Lecture - additional operators in sv

--- lectures/03.add_op/addop_test.sim/sim_1/behav/xsim/xsim.dir/addop_test_behav/obj/xsim_1.c
+++ lectures/03.add_op/addop_test.sim/sim_1/behav/xsim/xsim.dir/addop_test_behav/obj/xsim_1.c
@@ -55,9 +55,9 @@
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
 IKI_DLLESPEC extern void execute_2(char*, char *);
+IKI_DLLESPEC extern void execute_3(char*, char *);
 IKI_DLLESPEC extern void execute_4(char*, char *);
 IKI_DLLESPEC extern void execute_5(char*, char *);
-IKI_DLLESPEC extern void execute_6(char*, char *);
 IKI_DLLESPEC extern void execute_7(char*, char *);
 IKI_DLLESPEC extern void execute_8(char*, char *);
 IKI_DLLESPEC extern void execute_9(char*, char *);
@@ -65,13 +65,18 @@ IKI_DLLESPEC extern void execute_10(char*, char *);
 IKI_DLLESPEC extern void execute_11(char*, char *);
 IKI_DLLESPEC extern void execute_12(char*, char *);
 IKI_DLLESPEC extern void execute_13(char*, char *);
+IKI_DLLESPEC extern void execute_14(char*, char *);
+IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_16(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[12] = {(funcp)execute_2, (funcp)execute_4, (funcp)execute_5, (funcp)execute_6, (funcp)execute_7, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 12;
+IKI_DLLESPEC extern void transaction_3(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void vlog_transfunc_eventcallback_2state(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[17] = {(funcp)execute_2, (funcp)execute_3, (funcp)execute_4, (funcp)execute_5, (funcp)execute_7, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_16, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_3, (funcp)vlog_transfunc_eventcallback_2state};
+const int NumRelocateId= 17;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/addop_test_behav/xsim.reloc",  (void **)funcTab, 12);
+	iki_relocate(dp, "xsim.dir/addop_test_behav/xsim.reloc",  (void **)funcTab, 17);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
--- lectures/03.add_op/src/addop_test.sv
+++ lectures/03.add_op/src/addop_test.sv
@@ -21,6 +21,8 @@
 
 
 module addop_test();
+
+    // Wildcard equality operators
     logic [3:0] A = 4'b1111;
     logic [3:0] B = 4'bxxz1;
     logic [3:0] C = 4'bxxz0;
@@ -31,4 +33,38 @@ module addop_test();
         if (A !=? C)
             $display("A %b does not match with C %b", A, C);
     end
+    
+
+
+    // Inside operator
+    int a;
+    initial begin
+        a = 32;
+        #1 a = 4;
+        #1 a = 5;
+        #1 a = 4;
+    end
+
+    always @(*) begin
+        if (a inside{[1:8]})
+            $display("a = %d is inside 1..8", a);
+        else if (a inside {2, 4, 8, 16, 32})
+            $display("a = %d is inside {2, 4, 8, 16, 32}", a);
+    end
+
+    // Streaming operators (pack and unpack)
+    bit [4:0] x = 5'b10100;
+
+    initial begin
+        $display("x = %b", x);
+        $display("<<{x} = %b", {<<{x}});
+        $display(">>{x} = %b", {>>{x}});
+        $display("<<2{x} = %b", {<<2{x}});
+
+        $display("<<{8'b1000_1010} = %b", {<<{8'b1000_1010}});      // bit reverse
+        $display("<<4{6'b10_0010} = %b", {<<4{6'b10_0010}});        // 001010
+        $display("<<5{6'b10_0010} = %b", {<<5{6'b10_0010}});        // 000101
+        $display(">>5{6'b10_0010} = %b", {>>5{6'b10_0010}});        // 100010
+    end
+
 endmodule
