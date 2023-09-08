ae6d82f9a04e6d24c05b85d2641dc947195c422b
Trung Nguyen <nguyencanhtrung@me.com>
2023-09-08 09:57:28 +0700

    Lecture: packages

--- lectures/04.packages/src/packages.sv
+++ lectures/04.packages/src/packages.sv
@@ -6,3 +6,21 @@
 * Last Modified by:     Nguyen Canh Trung
 * Last Modified time:   2023-09-07 08:59:19
 * --------------------------------------------*/
+package my_package;
+    typedef enum {RED, GREEN, BLUE} color_t;
+    typedef struct {
+        int field_a;
+        color_t c;
+    } MyStruct;
+endpackage
+
+module my_module;
+import my_package::*;
+    MyStruct s;
+    initial begin
+        s.field_a = 1;
+        s.c = RED;
+
+        #1 $display("s.field_a = %d, s.c = %s", s.field_a, s.c.name());
+    end
+endmodule
